2974671bf5f27c2410c555a2ac95f0c7398a10f4
Imami Joel Betofe <jobetofeimami@gmail.com>
2026-04-28 13:42:21 +0200

    Remove more comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   timeout: 60000,
-  reporter: 'html',
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
